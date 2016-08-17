de7a015ce9ba23feb7db9e76db6ce3b94a9eeaa1
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-17 14:56:57 -0300

    Create controller

--- Controllers/app.js
+++ Controllers/app.js
@@ -1,1 +1,9 @@
-angular.module('app', []);
+// The first parameter of angular.module defines the name of the module.
+// The second parameter is an array that declares the module dependencies that our module uses. 
+var app = angular.module('app', []);
+
+// 'MainCtrl' is the controller name.
+// Second parameter is the function that represents our controller.
+app.controller('MainCtrl', function (){
+
+});
