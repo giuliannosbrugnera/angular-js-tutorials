e46956ffc7280807a9b29ef1fc41c782ff7d2e81
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-17 17:48:26 -0300

    Simulate reject the return promise

--- Promises/app.js
+++ Promises/app.js
@@ -1,21 +1,25 @@
 function getData($timeout, $q) {
-	return function() {
-		var defer = $q.defer()
+    return function() {
+        var defer = $q.defer()
 
-		// simulated async function
-		$timeout(function() {
-			defer.resolve('data received!')
-		}, 2000)
+        // simulated async function
+        $timeout(function() {
+            if (Math.round(Math.random())) {
+                defer.resolve('data received!')
+            } else {
+                defer.reject('oh no an error! try again')
+            }
+        }, 2000)
 
-		return defer.promise
-	}
+        return defer.promise
+    }
 }
 
 angular.module('app', [])
-	.factory('getData', getData)
-	.run(function(getData) {
-		var promise = getData()
-			.then(function(string) {
-				console.log(string)
-			})
-	})
+    .factory('getData', getData)
+    .run(function(getData) {
+        var promise = getData()
+            .then(function(string) {
+                console.log(string)
+            })
+    })
