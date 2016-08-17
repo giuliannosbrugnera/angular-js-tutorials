df40dede3267d89da778d61cc7d8930eb5ae6014
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-17 16:06:56 -0300

    Create a form

--- Services/app.js
+++ Services/app.js
@@ -3,34 +3,34 @@ var app = angular.module('app', []);
 
 // Service called 'messages'
 app.factory('messages', function() {
-	var messages = {};
+    var messages = {};
 
-	messages.list = [];
+    messages.list = [];
 
-	messages.add = function(message) {
-		messages.list.push({
-			id: messages.list.length,
-			text: message
-		});
-	};
+    messages.add = function(message) {
+        messages.list.push({
+            id: messages.list.length,
+            text: message
+        });
+    };
 
-	return messages;
+    return messages;
 });
 
 // Controller called 'ListCtrl' that injects our messages 
 // service, and exposes the list from our service to our view.
 app.controller('ListCtrl', function(messages) {
-	var self = this;
+    var self = this;
 
-	self.messages = messages.list;
+    self.messages = messages.list;
 });
 
 // Controller named 'PostCtrl' that also injects our messages service.
 // This controller will also contain an 'addMessage' function that uses the add function we made in our service.
 app.controller('PostCtrl', function(messages) {
-	var self = this;
+    var self = this;
 
-	self.addMessage = function(message) {
-		messages.add(message);
-	};
+    self.addMessage = function(message) {
+        messages.add(message);
+    };
 });
