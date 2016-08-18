e0c60ad76b46f989f3981a93f1ed0cb7a76a487e
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 13:21:06 -0300

    Create directive for the mouse leaving the element

--- Directives/Functionalities/app.js
+++ Directives/Functionalities/app.js
@@ -11,5 +11,16 @@ function entering() {
     }
 }
 
+function leaving() {
+    return function(scope, element) {
+        // Bind “mouseleave”.
+        element.bind("mouseleave", function() {
+            // Log “Mouse has left the div” to the console.
+            console.log("Mouse has left the div");
+        })
+    }
+}
+
 var functionalities = angular.module('functionalities', []);
 functionalities.directive("entering", entering);
+functionalities.directive("leaving", leaving);
