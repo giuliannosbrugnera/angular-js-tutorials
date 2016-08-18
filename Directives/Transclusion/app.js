3b936b204588a5da8c4ad557b09fb15410395a2e
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 15:03:55 -0300

    Fixed missing ng-transclude directive

--- Directives/Transclusion/app.js
+++ Directives/Transclusion/app.js
@@ -6,7 +6,7 @@ function welcome() {
         // Meaning that the <button> element will be removed entirely.
         // If the content within the panel element is intended to persist, the directive needs to have transclusion enabled:
         transclude: true,
-        template: "<div>This is the welcome component</div>"
+        template: "<div>This is the welcome component</div><ng-transclude></ng-transclude>"
     }
 }
 
