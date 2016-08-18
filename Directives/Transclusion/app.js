4556babc7401b20a8cc174e39de9915b2a76036c
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 14:56:37 -0300

    Enable transclusion within the "welcome" directive

--- Directives/Transclusion/app.js
+++ Directives/Transclusion/app.js
@@ -2,6 +2,10 @@ function welcome() {
     return {
         restrict: "E",
         scope: {},
+        // The default for directives is to destructively replace the contents of the element.
+        // Meaning that the <button> element will be removed entirely.
+        // If the content within the panel element is intended to persist, the directive needs to have transclusion enabled:
+        transclude: true,
         template: "<div>This is the welcome component</div>"
     }
 }
