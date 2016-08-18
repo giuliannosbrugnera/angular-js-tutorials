4dd2a8a1460632b8d3d39f3ab8faec9fca2faab0
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 17:19:11 -0300

    Add a containter directive now

--- Directives/Components and Containers/app.js
+++ Directives/Components and Containers/app.js
@@ -1,3 +1,4 @@
+// A component directive - uncomplicated behavior, essentially display the data passed into the attributes.
 function clock() {
     return {
         restrict: 'E',
@@ -6,6 +7,22 @@ function clock() {
         },
         template: "<div>12:00pm {{timezone}}</div>"
     }
+}
+
+// A container directive.
+// Panel takes a title attribute from the view, uses @ in the isolate scope to insert it into the
+// template, and uses transclusion to append a clock component from the view into the template after the alert-box title.
+function panel() {
+    return {
+        restrict: "E",
+        transclude: true,
+        scope: {
+            title: "@"
+        },
+        template: "<div style='border: 3px solid #000000'>" +
+            "<div class='alert-box'>{{title}}</div>" +
+            "<div ng-transclude></div></div>"
+    };
 }
 
 var app = angular.module("app", []);
