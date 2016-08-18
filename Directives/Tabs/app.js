ec8e54e779560b39a19174bea158eb3aea5e7edb
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 15:34:15 -0300

    Define a new directive called 'tab'
    It returns a basic DDO (Directive Definition Object)

--- Directives/Tabs/app.js
+++ Directives/Tabs/app.js
@@ -1,8 +1,17 @@
 ;
 (function(window) {
 
-    angular.module('app', [])
+    function tab() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
+            scope: {},
+            link: function(scope, elem, attr) {}
+        }
+    }
 
-    // Define directives here
+    var app = angular.module('app', []);
+    app.directive('tab', tab);
 
 })(window);
