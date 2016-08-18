b31f8d9c3c5999cdfc39812a9bf9b33dd9d78543
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-18 12:41:34 -0300

    Code refactoring

--- Directives/app.js
+++ Directives/app.js
@@ -1,21 +1,26 @@
-angular.module('greetings', [])
-    // `welcome` is the name of the new directive. Second paramenter is a function for callback.
-    .directive("welcome", function() {
-        // Return an object.
-        return {
-            // Property that restricts this directive to `Elements`.
-            restrict: "E",
-            // Property for specifying the HTML for our directive .
-            template: "<div>Howdy there! You look splendid.</div>"
+function Welcome() {
+    // Return an object.
+    return {
+        // Property that restricts this directive to `Elements`.
+        restrict: "E",
+        // Property for specifying the HTML for our directive .
+        template: "<div>Howdy there! You look splendid.</div>"
+    }
+}
+
+function Howdy() {
+    // Return an object.
+    return {
+        // Property that restricts this directive to `Attribute`.
+        restrict: "A",
+        // Linking function
+        link: function() {
+            alert("Howdy!");
         }
-    })
-    .directive("howdy", function() {
-        return {
-            // Property that restricts this directive to `Attribute`.
-            restrict: "A",
-            // Linking function
-            link: function() {
-                alert("Howdy!");
-            }
-        }
-    });
+    }
+}
+
+
+var greetings = angular.module('greetings', []);
+greetings.directive("welcome", Welcome);
+greetings.directive("howdy", Howdy);
