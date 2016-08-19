c8da4b03ade51f92a47ec61b4e7b4069d5c9bd32
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-19 15:43:04 -0300

    Refactor

--- Element/app.js
+++ Element/app.js
@@ -1,12 +1,15 @@
 var app = angular.module("app", [])
 
 app.directive("dumbPassword", function() {
+  var validElement = angular.element('<div>{{ model.input }}</div>');
   return {
     restrict: "E",
     replace: true,
     templateUrl: "dumbpass.html",
     link: function(scope, element) {
+      // Sets a listener on that model and waits for it to change.
       scope.$watch("model.input", function(value) {
+        // When it changes, the value parameter can be used to check the new value in the listener function.
         if (value === "password") {
           element.children(1).toggleClass("alert-box alert");
         }
