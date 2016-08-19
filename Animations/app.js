b5e7d4b269c118b4b76e843ea101b69350d5aa3e
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-19 17:16:27 -0300

    Animate entrances & exits of ng-repeat elements

--- Animations/app.js
+++ Animations/app.js
@@ -3,6 +3,15 @@ function TestCtrl() {
 
   self.showBoxOne = false;
   self.showBoxTwo = false;
+  // initialize an array
+  // buttons in html add/rm items in this array
+  self.people = [{
+    name: "Eric Simons"
+  }, {
+    name: "Albert Pai"
+  }, {
+    name: "Matthew Greenster"
+  }];
 }
 
 var app = angular.module('app', ['ngAnimate']);
