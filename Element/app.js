0cf1bd4301acb385ab8588634c3de8509e67ac54
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-19 15:17:52 -0300

    Initial setup for "Element" lesson

---
+++ Element/app.js
@@ -0,0 +1,16 @@
+var app = angular.module("app", [])
+
+app.directive("dumbPassword", function() {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "dumbpass.html",
+    link: function(scope, element) {
+      scope.$watch("model.input", function(value) {
+        if (value === "password") {
+          element.children(1).toggleClass("alert-box alert");
+        }
+      });
+    }
+  };
+});
