2838fc85d423a515713689a4fa614dda061361f8
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-19 15:12:50 -0300

    Wrap up of Directives lessons

--- Directives/Isolate Scope/Review/app.js
+++ Directives/Isolate Scope/Review/app.js
@@ -10,11 +10,15 @@ app.directive('phone', function() {
     return {
         restrict: 'E',
         scope: {
+            // Basically shorthand for reading in an attribute.
             number: '@',
+            // Set up a bidirectional two-way binding.
+            // Anything we update on the directive will be updated in the controller as well.
             network: '=',
+            // Calling something within the scope of that controller.
             makeCall: '&'
         },
-        templateUrl: 'phone.html'
+        templateUrl: 'phone.html',
         link: function(scope) {
             scope.networks = ["Verizon", "AT&T", "Sprint"];
             scope.network = scope.networks[0];
