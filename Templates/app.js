362127be0bf171a108818c3ff2dcb8251f6b4669
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-22 09:40:58 -0300

    Populate $templateCache directly

--- Templates/app.js
+++ Templates/app.js
@@ -18,4 +18,9 @@
     })
     .controller('TestCtrl', TestCtrl);
 
+  // Since $templateCache is a service, we won't be able to access it in the config phase of our app.
+  // The earliest we can access $templateCache is in the run phase for our app. 
+  angular.module('app').run(function($templateCache) {
+    $templateCache.put('test.html', 'Hello {{ test.user.name }}!');
+  });
 })()
