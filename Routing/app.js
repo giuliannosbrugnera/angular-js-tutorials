7388cdaab8f49d5effe6d66af78deee244aff936
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-22 10:54:39 -0300

    Pass a function to redirectTo instead of a route

--- Routing/app.js
+++ Routing/app.js
@@ -6,8 +6,21 @@ app.config(function($routeProvider) {
             controller: "AppCtrl",
             controllerAs: "app"
         })
-        .otherwise({
-            redirectTo: "/"
+        .when('/cookies/:cookieType', {
+            // The routeParams object is the same as the $routeParams used in the controller.
+            // 'path' is the string path the router received.
+            // 'search' is the key/value set of the query string parameters.
+            redirectTo: function(routeParams, path, search) {
+                console.log(routeParams);
+                console.log(path);
+                console.log(search);
+            // When the router sees the '/cookies' route, this will switch off to the '/sugar' route
+            // when the :cookieType route parameter is 'sugar'.
+                return "/" + routeParams.cookieType;
+            }
+        })
+        .when('/sugar', {
+            template: 'Sugar cookie'
         });
 })
 
