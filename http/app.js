0d8c1bf036f0404028599157ab282b0db6cff6ed
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-22 13:16:05 -0300

    GET-in data

--- http/app.js
+++ http/app.js
@@ -1,12 +1,24 @@
 function testService($http) {
-  this.get = function() {}
+  this.get = function() {
+    // '$http' methods return Angular '$q' promises, which allows for clean code flow via promise chaining.
+    return $http.get('http://test-routes.herokuapp.com/test/hello')
+      .then(function(res) {
+        // Return the enveloped data.
+        return res.data.message;
+      });
+  }
 }
 
 
 function TestCtrl(testService) {
   var self = this;
 
-  self.getMessage = function() {}
+  self.getMessage = function() {
+    testService.get()
+      .then(function(message) {
+        self.message = message;
+      })
+  }
 }
 
 var app = angular.module('app', []);
