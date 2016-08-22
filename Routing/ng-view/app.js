bce4e27540d71f589325ad2afb2c2d4e0ddc7eb0
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-22 10:36:23 -0300

    $routeProvider API methods

--- Routing/ng-view/app.js
+++ Routing/ng-view/app.js
@@ -1,14 +1,22 @@
 var app = angular.module("app", ["ngRoute"]);
 
+// $routeProvider has a simple API, accepting either the when() method, which matches a pattern, or otherwise().
+// It also allows for method chaining.
 app.config(function($routeProvider) {
     // Configure a route.
     // First parameter: specify the route.
     // Second paramenter: provide an object with a templateUrl property and a controller property.
     $routeProvider.when("/", {
-        templateUrl: "app.html",
-        controller: "AppCtrl",
-        controllerAs: "app"
-    });
+            templateUrl: "app.html",
+            controller: "AppCtrl",
+            controllerAs: "app"
+        })
+        .when('/cookies', {
+            template: "NOM NOM NOM NOM"
+        })
+        .otherwise({
+            template: "This route isn't set!"
+        });
 });
 
 // Create a controller to wire up to the route.
