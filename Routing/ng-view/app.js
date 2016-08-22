dd4c463ce7642f9503a39056f93298934f6a0ca1
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-22 10:42:16 -0300

    $routeParams lesson

--- Routing/ng-view/app.js
+++ Routing/ng-view/app.js
@@ -1,27 +1,17 @@
 var app = angular.module("app", ["ngRoute"]);
 
-// $routeProvider has a simple API, accepting either the when() method, which matches a pattern, or otherwise().
-// It also allows for method chaining.
+// $routeProvider will match the route against /:message, and the value of :message
+// can be injected into the controller as $routeParams, and retrieved with a key of the same name.
+// The route parameters can be stacked and accessed in the same way.
 app.config(function($routeProvider) {
-    // Configure a route.
-    // First parameter: specify the route.
-    // Second paramenter: provide an object with a templateUrl property and a controller property.
-    $routeProvider.when("/", {
-            templateUrl: "app.html",
-            controller: "AppCtrl",
-            controllerAs: "app"
-        })
-        .when('/cookies', {
-            template: "NOM NOM NOM NOM"
-        })
-        .otherwise({
-            template: "This route isn't set!"
-        });
-});
+    $routeProvider.when("/:firstName/:middleName/:lastName", {
+        templateUrl: "app.html",
+        controller: "AppCtrl",
+        controllerAs: "app"
+    });
+})
 
-// Create a controller to wire up to the route.
-app.controller('AppCtrl', function() {
-    // Create a model on the scope with a message.
+app.controller('AppCtrl', function($routeParams) {
     var self = this;
-    self.message = "The app routing is working!";
+    self.message = $routeParams.firstName + " " + $routeParams.middleName + " " + $routeParams.lastName;
 });
