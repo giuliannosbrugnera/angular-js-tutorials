8f372f67cb91af88a5ac84d35d5763203b10d9e9
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-23 13:05:19 -0300

    Add explanation comment

--- $scope vs scope/app.js
+++ $scope vs scope/app.js
@@ -1,3 +1,6 @@
+// If you open your developer console and refresh the page, you should see both $scope and scope.
+// Notice that they both have a $id property with the same value, which means that these two variables
+// actually refer to the exact same scope object.
 function TestCtrl($scope) {
   console.log($scope);
 }
